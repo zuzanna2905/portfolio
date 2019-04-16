2058526efadd1aa90aa6667175ea3ee93e08142f
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-04-16 14:19:14 +0200

    Projects changed to class

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import { Route, Link } from "react-router-dom";
 
 const Farmer = () => (
@@ -239,65 +239,79 @@ const RoboFriends = () => (
   </div>
 );
 
-const Projects = ({ match }) => (
-  <div className='washed-blue ma1 pa1'>
-    <ul className='list flex flex-wrap justify-center f5-ns f6'>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/farmer`}>Farmer</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/spendings`}>Spendings</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/cafes`}>Cafes</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/coffee`}>Coffee</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/taskmanager`}>Task Manager</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/travels`}>Travles</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/blog`}>Blog</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/smartbrain`}>Smart Brain</Link>
-        </li>
-        <li className='br2 ba b--white-30 justify-center pa2 mh3 mv1 w-20-ns w-40'>
-          <Link to={`${match.url}/robofriends`}>Robots Friends</Link>
-        </li>
-      </ul>
-      
-    <div className='f4-ns f5 mh4 pv2 washed-blue justify-center shadow-4'>
-      <Route path={`${match.url}/farmer`} component={Farmer} />
-      <Route path={`${match.url}/spendings`} component={Spendings} />
-      <Route path={`${match.url}/taskmanager`} component={TaskManager} />
-      <Route path={`${match.url}/cafes`} component={Cafes} />
-      <Route path={`${match.url}/coffee`} component={Coffee} />
-      <Route path={`${match.url}/travels`} component={Travels} />
-      <Route path={`${match.url}/blog`} component={Blog} />
-      <Route path={`${match.url}/smartbrain`} component={SmartBrain} />
-      <Route path={`${match.url}/robofriends`} component={RoboFriends} />
-      <Route
-        exact
-        path={match.url}
-        render={() => 
-          <div className='white'>
-            <p className='fw7 dark-green'>About</p>
-            <p>Most of my latest projects based on ES6, HTML5, CSS3 and React/Redux</p>
-            <p>I use NodeJS and Express to backend side</p>
-            <p>I use PostgreSql to storage data</p>
-            <p>In some projects I add tests (Jest/ Mocha & Chai)</p>
-            <p>Some projects are dockerized</p>
-            <p className='fw7 purple'>Please select a project above</p>
-          </div>
-        }
-      />
-    </div>
-    </div>
-  );
+class Projects extends Component {
+  state = {
+    showProjects : false
+  }
+
+  showProjects = () => {
+    this.setState({showProjects: !this.state.showProjects})
+  }
+
+  render() {
+    const {match} = this.props;
+    const navigation = this.state.showProjects ? <ul className='list flex flex-wrap justify-center f5-ns f6'>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/farmer`}>Farmer</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/spendings`}>Spendings</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/cafes`}>Cafes</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/coffee`}>Coffee</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/taskmanager`}>Task Manager</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/travels`}>Travles</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/blog`}>Blog</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/smartbrain`}>Smart Brain</Link>
+      </li>
+      <li className='br2 ba b--white-30 justify-center pa2 mh3-ns mv1 w-10-l w-20-m'>
+        <Link to={`${match.url}/robofriends`}>Robots Friends</Link>
+      </li>
+    </ul> : null;
+    return (
+      <div className='washed-blue ma1 pa1'>
+        <button className='white br2 ba ma2 b--white-30 justify-center bg-transparent pa2 w-40' onClick={this.showProjects}>Show projects</button>
+        {navigation} 
+        <div className='f4-ns f5 mh4 pv2 washed-blue justify-center shadow-4'>
+          <Route path={`${match.url}/farmer`} component={Farmer} />
+          <Route path={`${match.url}/spendings`} component={Spendings} />
+          <Route path={`${match.url}/taskmanager`} component={TaskManager} />
+          <Route path={`${match.url}/cafes`} component={Cafes} />
+          <Route path={`${match.url}/coffee`} component={Coffee} />
+          <Route path={`${match.url}/travels`} component={Travels} />
+          <Route path={`${match.url}/blog`} component={Blog} />
+          <Route path={`${match.url}/smartbrain`} component={SmartBrain} />
+          <Route path={`${match.url}/robofriends`} component={RoboFriends} />
+          <Route
+            exact
+            path={match.url}
+            render={() => 
+              <div className='white'>
+                <p className='fw7 dark-green'>About</p>
+                <p>Most of my latest projects based on ES6, HTML5, CSS3 and React/Redux</p>
+                <p>I use NodeJS and Express to backend side</p>
+                <p>I use PostgreSql to storage data</p>
+                <p>In some projects I add tests (Jest/ Mocha & Chai)</p>
+                <p>Some projects are dockerized</p>
+                <p className='fw7 purple'>Please select a project above</p>
+              </div>
+            }
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
   export default Projects;
